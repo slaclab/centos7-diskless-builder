a86e55f7854d411b3e7a79a9bd67f9f3bdfed6ac
Jesus Vasquez <jesusvasquez333@gmail.com>
2021-09-03 16:51:39 -0700

    Add generation of the docker image to the generate.sh script

--- generate.sh
+++ generate.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Generate the builder docker image
+cd docker && ./build.sh && cd -
+
+# Use the docker image to build the CentOS7 diskless images
 docker container run -ti --rm \
 	--mount src=${PWD}/output,target=/output,type=bind \
     --mount src=${PWD}/scripts,target=/scripts,type=bind \
