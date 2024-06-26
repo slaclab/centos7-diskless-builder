508a915afe133ac2f5baa26380047c64e709049b
Daron Chabot <daron.chabot@gmail.com>
2024-06-26 16:22:18 -0700

    ENH: use max feature set of qemu host

--- run-qemu.sh
+++ run-qemu.sh
@@ -4,8 +4,9 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 qemu-system-x86_64 \
 	-m size=2048 \
+	-cpu max \
 	-nographic -no-reboot \
-	-kernel ./$(find output -iname "vmlinuz-*" | head -n 1) \
+	-kernel ./$(find output -iname "vmlinuz*" | head -n 1) \
 	-initrd ./output/Rocky9_Lite_dev_$(cat VERSION)_fs.cpio.gz \
 	-append "console=ttyS0 init=/init root=/dev/ram0"
 
