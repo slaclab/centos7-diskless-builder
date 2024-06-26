f5c0fc8e0ac35d89c7ca8ce16799af199b7f3157
Daron Chabot <daron.chabot@gmail.com>
2024-06-26 16:21:40 -0700

    STY: readability

--- login_docker.sh
+++ login_docker.sh
@@ -1,1 +1,3 @@
-docker run --rm --mount src=rl9-builder,target=/rl9-builder,type=volume -it rl9-builder bash
+docker run -it --rm \
+    --mount src=rl9-builder,target=/rl9-builder,type=volume \
+    rl9-builder bash
